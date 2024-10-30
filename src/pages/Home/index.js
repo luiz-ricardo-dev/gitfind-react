a4559b60179adb5e6af6058e38d29ce94807ac0a
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-10-30 16:54:11 -0400

    Foi implementado a Api do GitHub e implementado as funções de estado para exibir as informações dos perfils

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,31 @@
+import {useState} from 'react';
 import {Header} from "../../components/Header";
 import background from "../../assets/git2.png";
 import "./styles.css";
 import ItemList from '../../components/ItemList';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCorrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if(newUser.name){
+      const {avatar_url, name ,bio, login} = newUser;
+      setCorrentUser({avatar_url, name ,bio, login});
+    }
+
+    const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+    const newRepos = await reposData.json();
+
+    if(newRepos.length){
+      setRepos(newRepos);
+    }
+  };
+
   return (
     <div className="App">
         <Header />
@@ -12,24 +34,35 @@ function App() {
 
           <div className="info">
             <div>
-              <input name="usuario" placeholder="@username" />
-              <button>Buscar</button>
+              <input name="usuario" value={user} onChange={event => setUser(event.target.value)} placeholder="@username" />
+              <button onClick={handleGetData}>Buscar</button>
             </div>
-            <div className="perfil">
-              <img src="https://avatars.githubusercontent.com/u/87581372?v=4" className="profile" alt="Foto profile"/>
-              <div>
-                <h3>Luiz Ricardo</h3>
-                <span>@luiz-ricardo-dev</span>
-                <p>Desenvolvedor FullStack .NET/JAVA. Focado em criação de APIs e Microservices.</p>
+
+            {currentUser?.name ? (<>
+
+                <div className="perfil">
+                <img src={currentUser.avatar_url} className="profile" alt="Foto profile"/>
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
               </div>
-            </div>
-            <hr />
+              <hr />
+
+              </>) : null}
+              
+              {repos?.length ? (<>
+
             <div>
-              <h4>Repositórios</h4>
+              <h4 className="repositorio">Repositórios</h4>
               <ItemList title="Teste1" description="Teste de descrição" />
               <ItemList title="Teste1" description="Teste de descrição" />
               <ItemList title="Teste1" description="Teste de descrição" />
             </div>
+
+              </>) : null}
+
           </div>
 
         </div>
